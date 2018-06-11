566d6ed5d95c6be315b4caadaad6db33d364dbd5
adamw <adam@warski.org>
2018-06-11 21:51:56 +0200

    ZIo socket test

---
+++ core/src/test/scala/com/softwaremill/sockets/ZioSocketTest.scala
@@ -0,0 +1,14 @@
+package com.softwaremill.sockets
+
+import org.scalatest.concurrent.IntegrationPatience
+import org.scalatest.{BeforeAndAfterAll, FlatSpec}
+import scalaz.effect.RTS
+
+class ZioSocketTest extends FlatSpec with SocketTest with BeforeAndAfterAll with IntegrationPatience with RTS {
+
+  it should "distribute message and connect new clients" in {
+    runTest { socket =>
+      unsafePerformIOAsync[Nothing, Unit](UsingZio.router(socket))(_ => ())
+    }
+  }
+}
